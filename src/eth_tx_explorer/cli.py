38bb7c6a35b380a670fed0aaa1c5774444e50b17
Phoenix-L <lianghaitian@hotmail.com>
2025-12-17 15:43:16 +0800

    Replace stub to real codes for tx and block in inspect in cli.py (#2)
    * Replace stub to real codes for tx and block in inspect in cli.py
    
    * latest changes on requirements.lock.txt and requirements.txt

--- src/eth_tx_explorer/cli.py
+++ src/eth_tx_explorer/cli.py
@@ -1,4 +1,6 @@
 # src/eth_tx_explorer/cli.py
+from datetime import datetime
+from eth_tx_explorer.rpc import get_web3
 import click
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
@@ -25,13 +27,69 @@ def inspect(tx_hash: str | None, block: int | None) -> None:
     if tx_hash and block is not None:
         raise click.UsageError("Provide either TX_HASH or --block, not both.")
 
+    w3 = get_web3()
+
     if block is not None:
-        click.echo(f"[stub] would inspect block: {block}")
+        blk = w3.eth.get_block(block)
+
+        ts = datetime.utcfromtimestamp(blk.timestamp)
+
+        click.echo(f"Block: {blk.number}")
+        click.echo(f"Timestamp (UTC): {ts}")
+        click.echo(f"Transaction count: {len(blk.transactions)}")
+
         return
 
     if tx_hash:
-        click.echo(f"[stub] would inspect tx: {tx_hash}")
-        return
+        tx = w3.eth.get_transaction(tx_hash)
+
+        blk = w3.eth.get_block(tx.blockNumber)
+     
+        receipt = w3.eth.get_transaction_receipt(tx_hash)
+        value_eth = w3.from_wei(tx.value, "ether")
+        gas_fee_eth = w3.from_wei(
+            receipt.gasUsed * receipt.effectiveGasPrice,
+            "ether"
+        )
+
+        ts = datetime.utcfromtimestamp(blk.timestamp)
+
+        click.echo(f"Transaction: {tx_hash}")
+        click.echo(f"From: {tx['from']}")
+        click.echo(f"To: {tx.to}")
+        click.echo(f"Value: {value_eth} ETH")
+        click.echo(f"Gas used: {receipt.gasUsed}")
+        click.echo(f"Fee: {gas_fee_eth} ETH")
+        click.echo(f"Status: {'SUCCESS' if receipt.status == 1 else 'REVERTED'}")
+        click.echo(f"Block: {tx.blockNumber}")
+        click.echo(f"Timestamp (UTC): {ts}")
+  
+    elif tx_hash==None:
+        block = w3.eth.get_block("latest", full_transactions=True)
+        click.echo(f"Block {block.number} has {len(block.transactions)} txs")
+        for tx in block.transactions:
+            blk = w3.eth.get_block(tx.blockNumber)
+     
+            receipt = w3.eth.get_transaction_receipt(tx.hash)
+            value_eth = w3.from_wei(tx.value, "ether")
+            gas_fee_eth = w3.from_wei(
+                receipt.gasUsed * receipt.effectiveGasPrice,
+                "ether"
+            )
+
+            ts = datetime.utcfromtimestamp(blk.timestamp)
+
+            click.echo(f"Transaction: {tx.hash}")
+            click.echo(f"From: {tx['from']}")
+            click.echo(f"To: {tx.to}")
+            click.echo(f"Value: {value_eth} ETH")
+            click.echo(f"Gas used: {receipt.gasUsed}")
+            click.echo(f"Fee: {gas_fee_eth} ETH")
+            click.echo(f"Status: {'SUCCESS' if receipt.status == 1 else 'REVERTED'}")
+            click.echo(f"Block: {tx.blockNumber}")
+            click.echo(f"Timestamp (UTC): {ts}")
+        
+    return
 
     raise click.UsageError("Provide TX_HASH or --block.")
 
